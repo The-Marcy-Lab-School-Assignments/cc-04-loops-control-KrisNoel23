a96f37c6e293bd79abd6d7f73f3ea8360180bdaf
Kristopher Noel <noelkris500@gmail.com>
2024-10-08 11:11:42 -0400

    I got up to question 4

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 //Write your solutions in this file, don't forget to test your functions.
+/*const countFromOne = (n) => {
+
+    for(let i = 1; i <= n; i++) {
+
+        console.log(i);
+    }
+
+};
+
+countFromOne(10);
+*/
+/*const countEveryOdd = (n) => {
+    for (i = 1; i <= n; i++) {
+
+        if (i % 2 !== 0) {
+
+            console.log(i);
+        }
+    
+    }
+}
+countEveryOdd(15);
+*/
+
+/*const isNegative = (i) => {
+    if (i < 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
+};
+isNegative(-8);
+console.log(isNegative(-8));
+*/
+
+const betweenFiveAndTwenty = (num) => {
+    console.log(num >= 5 && num <= 20);
+};
+betweenFiveAndTwenty(100);
